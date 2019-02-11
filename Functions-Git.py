cb75aef7f837a84404bf93b644fe7c7ba0070ab3
Matthew Beaver <02matthew.beaver@gulllakecs.org>
2019-02-11 13:01:33 -0500

    Add the Functions & Variables

--- Functions-Git.py
+++ Functions-Git.py
@@ -1,7 +1,9 @@
-4.13.3: Greetings
+#4.13.3: Greetings
 #Matthew Beaver
 #2/5/19
 
+
+'''
 name = input("What is your name: ")
 
 def greeting():
@@ -9,3 +11,18 @@ def greeting():
     print("Nice to meet you")
 
 greeting()
+'''
+
+
+#4.13.4: Functions and Variables
+#Matthew Beaver
+#2.11.19
+
+x = 406
+
+def print_something():
+    x = 3
+    print('\n', x)
+
+print('\n', x)
+print_something()
